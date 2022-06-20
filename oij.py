fd442e829004309091be7ccce2ebbe9f7b8442e6
João Vitor Dourado <106849008+PITBULLAVAJATO@users.noreply.github.com>
2022-06-20 19:20:27 -0400

    Add files via upload

---
+++ oij.py
@@ -0,0 +1,42 @@
+
+
+
+
+
+idade=int(input("Digite sua idade"))
+if idade>=18: #Caso for vdd
+            print("Maior de idade")
+else:#Caso for falso
+    print("Menor de idade")
+
+
+
+
+
+
+idade=int(input("Digite sua idade"))
+if idade==16: #Caso for vdd
+    print("Pode votar")
+else:
+    if idade>=16:
+            print("Pode Dirigir")
+    else:    
+        if idade<16:
+              print("apenas estude")
+
+
+
+
+
+
+
+ 
+idade=int(input("Digite sua idade"))
+if idade==16 and idade <18: #caso vdd
+            print("Pode votar")
+elif idade<16:
+            print("Pode Dirigir")
+else:    
+            print("apenas estude")
+ 
+
